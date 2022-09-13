685cc356d2e9a3c882d5c3cb23495e5b179b6cd8
Jyoti1010yadav <113248661+Jyoti1010yadav@users.noreply.github.com>
2022-09-14 00:04:03 +0530

    Add files via upload

---
+++ ASSIGNMENT-8.py
@@ -0,0 +1,85 @@
+# 1. Write a python script to print MySirG 5 times on the screen
+print(" MySirG 5 times on the screen")
+i=0
+while(i<5):
+    print(" MySirG ")
+    i+=1
+print()    
+
+# 2. Write a python script to print first 10 natural numbers
+print(" first 10 natural numbers ")
+i=1
+while(i<=10):
+    print(i,sep=" ")
+    i+=1
+print()  
+
+# 3. Write a python script to print first 10 natural numbers in reverse order
+print(" first 10 natural numbers in reverse order ")
+i=10
+while(i>0):
+    print(i,sep=" ")
+    i-=1
+print()    
+
+# 4. Write a python script to print first 10 odd natural numbers
+print(" first 10 odd natural numbers ")
+i=1
+while(i<20):
+    if i%2!=0:
+        print(i)
+    else:    
+        pass
+    i+=1
+print()
+
+# 5. Write a python script to print first 10 odd natural numbers in reverse order
+print("first 10 odd natural numbers in reverse order ")
+i=19
+while(i>0):
+    if i%2!=0:
+        print(i)
+    i-=1
+print()        
+
+# 6. Write a python script to print first 10 even natural numbers
+print(" first 10 even natural numbers ")
+i=1
+while(i<=20):
+    if(i%2==0):
+        print(i)
+    i+=1    
+print()
+
+# 7. Write a python script to print first 10 even natural numbers in reverse order
+print("first 10 even natural numbers in reverse order ")
+i=20
+while(i>0):
+    if i%2==0:
+        print(i)
+    i-=1 
+print()       
+
+# 8. Write a python script to print squares of first 10 natural numbers
+print(" squares first 10 natural numbers ")
+i=1
+while(i<=10):
+    print(i*i)
+    i+=1
+print()    
+
+# 9. Write a python script to print cubes of first 10 natural numbers
+print(" cubes of first 10 natural numbers ")
+i=1
+while(i<=10):
+    print(i*i*i)
+    i+=1
+print()    
+
+# 10. Write a python script to print first 10 multiples of 5
+print(" first 10 multiples of 5")
+i=1
+while(i<=10):
+    print(5*i)
+    i+=1
+print()    
